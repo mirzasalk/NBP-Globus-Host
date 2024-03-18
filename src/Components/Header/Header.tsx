186d1739b5340d5a6eacdf59e9a2e0c7db56c157
Mirza Salković <99550646+mirzasalk@users.noreply.github.com>
2024-03-18 20:44:01 +0100

    Update Header.tsx

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,25 +1,35 @@
 import "./header.css";
 import { useEffect,useState } from "react";
 import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import UserContext from "../../reactContext/UserContext";
+
 
 const Header = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+ 
+  const context = useContext(UserContext);
+  if (!context) {
+    // Ako kontekst nije definisan, možete postupiti na odgovarajući način, na primer, vratiti null ili nešto drugo
+    return null;
+  }
 
+  // Destrukturiranje podataka iz konteksta
+  const { islogin, setislogin } = context;
   useEffect(() => {
     // Provera da li postoji token u localStorage
     const token = localStorage.getItem("token");
-    setIsLoggedIn(!!token); // Ako token postoji, postavljamo isLoggedIn na true
+    setislogin(!!token); // Ako token postoji, postavljamo isLoggedIn na true
   }, []);
 
   const handleLogout = () => {
     // Brisanje tokena iz localStorage-a prilikom odjave
     localStorage.removeItem("token");
-    setIsLoggedIn(false);
+    setislogin(false);
   };
   return (
     <div id="headerMain">
       <img className="logoImg" src="./globusLogo.png" alt="logo" />
-        {isLoggedIn ? (
+        {islogin ? (
           // Ako je korisnik ulogovan, prikaži Logout link
           <div className="logIn-LogOutDiv">
           <a href="/" onClick={handleLogout}>Logout</a>
